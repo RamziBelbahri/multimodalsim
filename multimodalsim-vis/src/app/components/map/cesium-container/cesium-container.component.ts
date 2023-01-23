9a6a3791635fd2df64e8dac5847a33a9096266f0
Pierre-1Paquette <ppaquette2000@gmail.com>
2023-01-23 18:08:14 -0500

    callback pour déplacer

--- multimodalsim-vis/src/app/components/map/cesium-container/cesium-container.component.ts
+++ multimodalsim-vis/src/app/components/map/cesium-container/cesium-container.component.ts
@@ -26,10 +26,14 @@ export class CesiumContainerComponent {
 		}
 	}
 
-	testEntitySpawn() {
+	getNewPosition() {
+		return new Cesium.PolygonHierarchy(new Cesium.Cartesian3.fromDegreesArray([-75.751564, 45.576321, -75.754564, 45.576321, -75.754564, 45.579321, -75.751564, 45.579321]));
+	}
+
+	testEntitySpawn(): void {
 		this.viewer.entities.add({
 			polygon: {
-				hierarchy: Cesium.Cartesian3.fromDegreesArray([-73.751564, 45.576321, -73.754564, 45.576321, -73.754564, 45.579321, -73.751564, 45.579321]),
+				hierarchy: new Cesium.CallbackProperty(this.getNewPosition, false),
 				height: 0,
 				material: Cesium.Color.BLUE,
 				outline: true,
