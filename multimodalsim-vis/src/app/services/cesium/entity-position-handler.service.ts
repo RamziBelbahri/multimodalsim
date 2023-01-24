6069faf3d83f2770b1d2714e1b7567f77ac6648d
Pierre-1Paquette <ppaquette2000@gmail.com>
2023-01-24 18:50:01 -0500

    fonctionnel déplacement

--- multimodalsim-vis/src/app/services/cesium/entity-position-handler.service.ts
+++ multimodalsim-vis/src/app/services/cesium/entity-position-handler.service.ts
@@ -1,16 +1,41 @@
 import { Injectable } from '@angular/core';
-import { Entity } from 'cesium';
+import { Cartesian3, Entity } from 'cesium';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class EntityPositionHandlerService {
-	computeMovementTicks(/*entity: Entity, endPos: Array<number>, endTime: JulianDate*/) {
-		// Prendre la position finale désirée et trouver le nombre de tick de setEntityToNewPos qu'il faut faire pour séparer l'exécution sur le temps.
-		// TODO: trouver un moyen d'exécuter plusieurs fois setEntityToNewPos sans bloquer la performance
+	entity: Entity | undefined;
+	points: Array<Cartesian3>;
+	isChanged = false;
+
+	constructor() {
+		this.points = [
+			new Cesium.Cartesian3.fromDegrees(-73.751564, 45.576321),
+			new Cesium.Cartesian3.fromDegrees(-73.754564, 45.576321),
+			new Cesium.Cartesian3.fromDegrees(-73.754564, 45.579321),
+			new Cesium.Cartesian3.fromDegrees(-73.751564, 45.579321),
+		];
 	}
 
-	setEntityToNewPos(entity: Entity, newPos: Array<number>): void {
-		entity.polygon?.hierarchy?.getValue(Cesium.JulianDate.now(), newPos);
+	startComputation(entity: Entity | undefined): void {
+		this.entity = entity;
+
+		setInterval(() => {
+			if (this.entity?.polygon?.hierarchy) {
+				if (this.isChanged) {
+					this.entity.polygon.hierarchy = new Cesium.PolygonHierarchy(this.points);
+					this.isChanged = false;
+				}
+			}
+		}, 100);
+	}
+
+	updateEntityPos(increments: Array<Cartesian3>): void {
+		for (let i = 0; i < increments.length; i++) {
+			this.points[i].x += increments[i].x;
+		}
+
+		this.isChanged = true;
 	}
 }
