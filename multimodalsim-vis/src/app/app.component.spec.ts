ff6c28b90b2c25c5795c40efddd49bce9cfc3bc5
Pierre-1Paquette <ppaquette2000@gmail.com>
2023-01-26 12:20:19 -0500

    encapsulation des new de Cesium

--- multimodalsim-vis/src/app/app.component.spec.ts
+++ multimodalsim-vis/src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CesiumContainerComponent } from './components/map/cesium-container/cesium-container.component';
 
 describe('AppComponent', () => {
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			declarations: [AppComponent],
+			declarations: [AppComponent, CesiumContainerComponent],
 		}).compileComponents();
 	});
 
@@ -24,8 +25,6 @@ describe('AppComponent', () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		fixture.detectChanges();
 		const compiled = fixture.nativeElement as HTMLElement;
-		expect(compiled.querySelector('.content span')?.textContent).toContain(
-			'multimodalsim-vis app is running!'
-		);
+		expect(compiled.querySelector('.content span')?.textContent).toContain('multimodalsim-vis app is running!');
 	});
 });
