87b31144b60b1dcabe1e2b446e31329940a7c4f0
UrsuleJoelle <70718316+UrsuleJoelle@users.noreply.github.com>
2023-01-29 04:12:16 -0500

    basic dashboard ui

--- multimodalsim-vis/src/app/app.module.ts
+++ multimodalsim-vis/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CesiumContainerComponent } from './components/map/cesium-container/cesium-container.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
-	declarations: [AppComponent, CesiumContainerComponent],
+	declarations: [AppComponent, CesiumContainerComponent, DashboardComponent],
 	imports: [BrowserModule],
 	providers: [],
 	bootstrap: [AppComponent],
