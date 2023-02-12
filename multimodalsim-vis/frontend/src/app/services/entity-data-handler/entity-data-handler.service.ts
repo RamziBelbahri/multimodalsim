c606aa0d8dfd3c9d39080fe550c0682212bc3b98
Rui Jie Li <rui-jie.li@polymtl.ca>
2023-02-11 20:46:42 -0500

    changed added some functions for passengers

--- multimodalsim-vis/frontend/src/app/services/entity-data-handler/entity-data-handler.service.ts
+++ multimodalsim-vis/frontend/src/app/services/entity-data-handler/entity-data-handler.service.ts
@@ -1,28 +1,36 @@
 import { Injectable } from '@angular/core';
 import { BusEvent } from 'src/app/classes/bus-class/bus-event';
+import { PassengerEvent } from 'src/app/classes/passenger-event/passenger-event';
 import { getTime } from 'src/app/helpers/parsers';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class EntityDataHandlerService {
-	private data: BusEvent[];
+	private busData: BusEvent[];
+	private passengerData:PassengerEvent[];
 	private clock: number;
 
 	constructor() {
-		this.data = [];
+		this.busData = [];
+		this.passengerData = [];
 		this.clock = 0;
 	}
 
-	setData(data: BusEvent[]): void {
-		this.data = data;
+	setBusData(busData: BusEvent[]): void {
+		this.busData = busData;
 		this.runVehiculeSimulation();
+	}
+
+	setPassengerData(passengerData:PassengerEvent[]):void {
+		this.passengerData = passengerData;
+		this.runPassengerSimulation()
 	}
 
 	// runs the simulation, if the clock is equal
 	runVehiculeSimulation() {
-		console.log(this.data);
-		for (const event of this.data) {
+		console.log(this.busData);
+		for (const event of this.busData) {
 			if (event) {
 				console.log(event);
 				const delay = this.getDelay(event);
@@ -32,7 +40,20 @@ export class EntityDataHandlerService {
 		}
 	}
 
-	getDelay(event: BusEvent) {
+	// runs the simulation, if the clock is equal
+	runPassengerSimulation() {
+		console.log(this.passengerData);
+		for (const event of this.passengerData) {
+			if (event) {
+				console.log(event);
+				const delay = this.getDelay(event);
+				console.log(delay);
+				this.clock = getTime(event.time);
+			}
+		}
+	}
+
+	getDelay(event: BusEvent|PassengerEvent) {
 		return getTime(event.time) - this.clock;
 	}
 }
