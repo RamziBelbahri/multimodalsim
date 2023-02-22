8aa8f2af8ba7c24314709835a91a3a4083695feb
Rui Jie Li <liruijie1999@gmail.com>
2023-02-21 19:36:17 -0500

    added visualizer and observer

--- multimodalsim-vis/backend/communication/active_mq_controller.py
+++ multimodalsim-vis/backend/communication/active_mq_controller.py
@@ -1,13 +1,15 @@
 from stomp import Connection11
 
 class ActiveMQController:
+	connection = None
 	def __init__(self) -> None:
 		pass
 	
 	def getConnection(user='admin',password='admin',host='0.0.0.0',port=61613):
-		conn = Connection11([(host, port)])  
-		conn.connect(user, password)
-		return conn
+		if ActiveMQController.connection == None:
+			ActiveMQController.connection = Connection11([(host, port)])  
+			ActiveMQController.connection.connect(user, password)
+		return ActiveMQController.connection
 
 if __name__ == '__main__':
 	a = ActiveMQController().getConnection()
