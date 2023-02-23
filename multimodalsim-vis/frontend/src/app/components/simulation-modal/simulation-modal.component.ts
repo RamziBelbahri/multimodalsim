aebbc876bc14183799b2dd3690c0398e3548ede9
Pierre-1Paquette <ppaquette2000@gmail.com>
2023-02-22 19:16:48 -0500

    subscribe - observer

--- multimodalsim-vis/frontend/src/app/components/simulation-modal/simulation-modal.component.ts
+++ multimodalsim-vis/frontend/src/app/components/simulation-modal/simulation-modal.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-import { EntityDataHandlerService } from 'src/app/services/entity-data-handler/entity-data-handler.service';
 import { SimulationParserService } from 'src/app/services/simulation-parser/simulation-parser.service';
 import { ZipHandlerComponent } from 'src/app/components/zip-handler/zip-handler.component';
+import { Viewer } from 'cesium';
+import { Subscription } from 'rxjs';
+import { CesiumClass } from 'src/app/shared/cesium-class';
+import { ViewerSharingService } from 'src/app/services/viewer-sharing/viewer-sharing.service';
 
 @Component({
 	selector: 'app-simulation-modal',
@@ -9,19 +12,24 @@ import { ZipHandlerComponent } from 'src/app/components/zip-handler/zip-handler.
 	styleUrls: ['./simulation-modal.component.css'],
 })
 export class SimulationModalComponent {
-	constructor(private simulationParserService: SimulationParserService, private entityDataHandlerService: EntityDataHandlerService) {}
+	private viewer: Viewer = CesiumClass.viewer('');
+	private viewerSubscription: Subscription = new Subscription();
+
+	constructor(private simulationParserService: SimulationParserService, private viewerSharer: ViewerSharingService) {}
+
+	ngOnInit() {
+		this.viewerSubscription = this.viewerSharer.currentViewer.subscribe((viewer) => (this.viewer = viewer));
+	}
 
 	selectFile(event: Event): void {
 		this.simulationParserService.selectFile(event);
 	}
 
 	readContent(): void {
-		const csvInput:HTMLInputElement = document.getElementById('csvinput') as HTMLInputElement;
-		if(csvInput.value != '')
-			this.simulationParserService.readFile();
-		const zipInput:HTMLInputElement = document.getElementById('zipInput') as HTMLInputElement;
-		if(zipInput.value != '')
-			ZipHandlerComponent.zipHandler.readZipContent();
+		const csvInput: HTMLInputElement = document.getElementById('csvinput') as HTMLInputElement;
+		if (csvInput.value != '') this.simulationParserService.readFile();
+		const zipInput: HTMLInputElement = document.getElementById('zipInput') as HTMLInputElement;
+		if (zipInput.value != '') ZipHandlerComponent.zipHandler.readZipContent();
 		this.closeModal();
 	}
 
