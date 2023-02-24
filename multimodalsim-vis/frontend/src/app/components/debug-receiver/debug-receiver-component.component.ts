9b84605920d58c79b9ea7ddbbf90dbae66a0c9e1
Rui Jie Li <liruijie1999@gmail.com>
2023-02-24 15:12:44 -0500

    changed the message queue service, maybe it will be turned into an interface

--- multimodalsim-vis/frontend/src/app/components/debug-receiver/debug-receiver-component.component.ts
+++ multimodalsim-vis/frontend/src/app/components/debug-receiver/debug-receiver-component.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import { CompatClient, IMessage } from '@stomp/stompjs';
+import { MessageQueueStompService } from 'src/app/services/messaging/message-queue-stomp.service';
+import { ConnectionCredentials } from 'src/app/services/messaging/connection-constants';
 @Component({
-  selector: 'app-debug-receiver-component',
-  templateUrl: './debug-receiver-component.component.html',
-  styleUrls: ['./debug-receiver-component.component.css']
+	selector: 'app-debug-receiver-component',
+	templateUrl: './debug-receiver-component.component.html',
+	styleUrls: ['./debug-receiver-component.component.css']
 })
 export class DebugReceiverComponentComponent {
-
+	private static client:CompatClient;
+	constructor() {
+		const service = new MessageQueueStompService();
+		DebugReceiverComponentComponent.client = service.getClient();
+		DebugReceiverComponentComponent.client.connect(ConnectionCredentials.USERNAME,ConnectionCredentials.PASSWORD,this.onConnect, this.onError)
+	}
+	onConnect() {
+		DebugReceiverComponentComponent.client.subscribe(ConnectionCredentials.EVENT_QUEUE, DebugReceiverComponentComponent.onMessage)
+	}
+	onError(err:IMessage){
+		console.log(err.body);
+	}
+	private static onMessage(msg:IMessage) {
+		const p = document.getElementById('received-text');
+		if(p) {
+			p.innerText = JSON.stringify(JSON.parse(msg.body), undefined, 2);
+		}
+	}
 }
--- multimodalsim-vis/frontend/src/app/services/messaging/message-queue-stomp.service.ts
+++ multimodalsim-vis/frontend/src/app/services/messaging/message-queue-stomp.service.ts
@@ -1,40 +1,26 @@
 import { Stomp, CompatClient, IMessage } from '@stomp/stompjs'
+import {ConnectionCredentials} from './connection-constants'
 
-const PING:string = "PING"
-const PONG:string = "PONG"
 // uses STOMP with active MQ
 export class MessageQueueStompService {
-	public socketAddress:string;
-	private readonly PROTOCOLS = ["v11.stomp"];
-
+	// public socketAddress:string;
 	public static client:CompatClient;
-	private static readonly HEADERS = {id: 'JUST.FCX', ack: 'client'};
-	private static readonly QUEUE = "/queue/server";
-	private static readonly WEBSOCKET = "ws://localhost:61614/stomp";
+	public static service:MessageQueueStompService;
+
 	// note: static is needed so that there the callbacks can work
-	constructor(socketAddress:string=MessageQueueStompService.WEBSOCKET, debug:boolean=false, username:string="admin", password:string="admin") {
-		this.socketAddress = socketAddress;
-		MessageQueueStompService.client = Stomp.client(socketAddress, this.PROTOCOLS);
+	constructor(socketAddress:string=ConnectionCredentials.WEBSOCKET, debug:boolean=true) {
+		if(MessageQueueStompService.service) {
+			return MessageQueueStompService.service;
+		}
+		MessageQueueStompService.client = Stomp.client(socketAddress, ConnectionCredentials.PROTOCOLS);
 		if(!debug){
 			MessageQueueStompService.client.debug = function() {};
 		}
-		MessageQueueStompService.client.connect(username,password, this.onConnect, this.onError);
+		MessageQueueStompService.service = this;
+		return this;
 	}
 
-	onConnect():void {
-		MessageQueueStompService.client.subscribe(MessageQueueStompService.QUEUE,MessageQueueStompService.onMessage)
-	}
-
-	static onMessage(msg:IMessage):void {
-		if(msg.body == PING) {
-			console.log("PING -- keeps the websocket open");
-			MessageQueueStompService.client.send(MessageQueueStompService.QUEUE, MessageQueueStompService.HEADERS, PONG);
-		}
-		console.log(msg.body)
-		// msg.ack();
-	}
-
-	onError(err:IMessage):void {
-		console.log(err.body)
+	getClient():CompatClient {
+		return MessageQueueStompService.client;
 	}
 }
