43e0f2d9925c3e88a54dff30082a7becf6121757
Rui Jie Li <liruijie1999@gmail.com>
2023-02-24 18:01:05 -0500

    added 3 other queues for event observations, trips and vehicles, plus a frontend data observer

--- multimodalsim-vis/backend/communication/active_mq_controller.py
+++ multimodalsim-vis/backend/communication/active_mq_controller.py
@@ -1,11 +1,11 @@
 from stomp import Connection11
-
+from connection_credentials import ConnectionCredentials
 class ActiveMQController:
 	connection = None
 	def __init__(self) -> None:
 		pass
 	
-	def getConnection(user='admin',password='admin',host='0.0.0.0',port=61613):
+	def getConnection(user=ConnectionCredentials.USERNAME,password=ConnectionCredentials.PASSWORD, host=ConnectionCredentials.HOST, port=ConnectionCredentials.PORT):
 		if ActiveMQController.connection == None:
 			ActiveMQController.connection = Connection11([(host, port)])  
 			ActiveMQController.connection.connect(user, password)
--- multimodalsim-vis/backend/communication/frontend_observer.py
+++ multimodalsim-vis/backend/communication/frontend_observer.py
@@ -1,13 +1,13 @@
 from multimodalsim.observer.environment_observer import EnvironmentObserver
-from multimodalsim.observer.data_collector import StandardDataCollector, DataContainer
+from multimodalsim.observer.data_collector import DataContainer
 from multimodalsim.statistics.data_analyzer import FixedLineDataAnalyzer
 
 from frontend_visualizer import FrontendVisualizer
-
+from frontend_data_collector import FrontendDataCollector
 class FrontendEnvironmentObserver(EnvironmentObserver):
     def __init__(self):
         data_container = DataContainer()
         super().__init__(
-            data_collectors=StandardDataCollector(data_container),
+            data_collectors=FrontendDataCollector(data_container),
             visualizers=FrontendVisualizer(data_analyzer=FixedLineDataAnalyzer(data_container))
         )
