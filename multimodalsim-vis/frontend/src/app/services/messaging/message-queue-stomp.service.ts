df34c65c36d52a3211acc1d7bca55b9fbcba86e9
Rui Jie Li <rui-jie.li@polymtl.ca>
2023-03-02 18:45:07 -0500

    tslint

--- multimodalsim-vis/frontend/src/app/services/messaging/message-queue-stomp.service.ts
+++ multimodalsim-vis/frontend/src/app/services/messaging/message-queue-stomp.service.ts
@@ -1,6 +1,6 @@
-import { Stomp, CompatClient, IMessage } from '@stomp/stompjs'
+import { Stomp, CompatClient, IMessage } from '@stomp/stompjs';
 import { EntityDataHandlerService } from '../entity-data-handler/entity-data-handler.service';
-import {ConnectionCredentials} from './connection-constants'
+import {ConnectionCredentials} from './connection-constants';
 
 // uses STOMP with active MQ
 export class MessageQueueStompService {
@@ -9,24 +9,24 @@ export class MessageQueueStompService {
 	public static service:MessageQueueStompService;
 
 	// note: static is needed so that there the callbacks can work
-	constructor(private entityDataHandlerService:EntityDataHandlerService, socketAddress:string=ConnectionCredentials.WEBSOCKET, debug:boolean=true) {
+	constructor(private entityDataHandlerService:EntityDataHandlerService, socketAddress:string=ConnectionCredentials.WEBSOCKET, debug=true) {
 		if(MessageQueueStompService.service) {
 			return MessageQueueStompService.service;
 		}
 		MessageQueueStompService.client = Stomp.client(socketAddress, ConnectionCredentials.PROTOCOLS);
 		if(!debug){
-			MessageQueueStompService.client.debug = function() {};
+			MessageQueueStompService.client.debug = function() {return;};
 		}
-		MessageQueueStompService.client.connect(ConnectionCredentials.USERNAME,ConnectionCredentials.PASSWORD,this.onConnect, this.onError)
+		MessageQueueStompService.client.connect(ConnectionCredentials.USERNAME,ConnectionCredentials.PASSWORD,this.onConnect, this.onError);
 		MessageQueueStompService.service = this;
 		return this;
 	}
 	onConnect() {
-		MessageQueueStompService.client.subscribe(ConnectionCredentials.INFO_QUEUE, MessageQueueStompService.onReceivingInfo)
-		MessageQueueStompService.client.subscribe(ConnectionCredentials.EVENT_QUEUE, MessageQueueStompService.onReceivingEvent)
-		MessageQueueStompService.client.subscribe(ConnectionCredentials.EVENTS_OBSERVATION_QUEUE, MessageQueueStompService.onReceivingEventObservation)
-		MessageQueueStompService.client.subscribe(ConnectionCredentials.TRIPS_QUEUE, MessageQueueStompService.onReceivingTripEvent)
-		MessageQueueStompService.client.subscribe(ConnectionCredentials.VEHICLE_QUEUE, MessageQueueStompService.onReceivingVehicleEvent)
+		MessageQueueStompService.client.subscribe(ConnectionCredentials.INFO_QUEUE, MessageQueueStompService.onReceivingInfo);
+		MessageQueueStompService.client.subscribe(ConnectionCredentials.EVENT_QUEUE, MessageQueueStompService.onReceivingEvent);
+		MessageQueueStompService.client.subscribe(ConnectionCredentials.EVENTS_OBSERVATION_QUEUE, MessageQueueStompService.onReceivingEventObservation);
+		MessageQueueStompService.client.subscribe(ConnectionCredentials.TRIPS_QUEUE, MessageQueueStompService.onReceivingTripEvent);
+		MessageQueueStompService.client.subscribe(ConnectionCredentials.VEHICLE_QUEUE, MessageQueueStompService.onReceivingVehicleEvent);
 	}
 	onError(err:IMessage){
 		console.log(err.body);
@@ -45,8 +45,8 @@ export class MessageQueueStompService {
 				p.innerText = JSON.stringify(JSON.parse(msg.body),undefined, 2);
 			} catch {
 				p.innerText = msg.body;
-				console.log('==========================================')
-				console.log(msg.body)
+				console.log('==========================================');
+				console.log(msg.body);
 			}
 		}
 	}
@@ -58,8 +58,8 @@ export class MessageQueueStompService {
 				p.innerText = JSON.stringify(JSON.parse(msg.body),undefined, 2);
 			} catch {
 				p.innerText = msg.body;
-				console.log('==========================================')
-				console.log(msg.body)
+				console.log('==========================================');
+				console.log(msg.body);
 			}
 		}
 	}
@@ -71,8 +71,8 @@ export class MessageQueueStompService {
 				p.innerText = JSON.stringify(JSON.parse(msg.body),undefined, 2);
 			} catch {
 				p.innerText = msg.body;
-				console.log('==========================================')
-				console.log(msg.body)
+				console.log('==========================================');
+				console.log(msg.body);
 			}
 		}
 	}
@@ -84,8 +84,8 @@ export class MessageQueueStompService {
 				p.innerText = JSON.stringify(JSON.parse(msg.body),undefined, 2);
 			} catch {
 				p.innerText = msg.body;
-				console.log('==========================================')
-				console.log(msg.body)
+				console.log('==========================================');
+				console.log(msg.body);
 			}
 		}
 	}
