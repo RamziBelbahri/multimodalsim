f36a6842805530895adf6698a3cee9e3e961e139
Pierre-1Paquette <ppaquette2000@gmail.com>
2023-03-04 17:41:11 -0500

    tous les arrêts en même temps

--- multimodalsim-vis/frontend/src/app/services/cesium/stop-position-handler.service.ts
+++ multimodalsim-vis/frontend/src/app/services/cesium/stop-position-handler.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Viewer } from 'cesium';
+import { Cartesian3, Viewer } from 'cesium';
 import { PassengerEvent } from 'src/app/classes/data-classes/passenger-event/passenger-event';
 import { PassengersStatus } from 'src/app/classes/data-classes/passenger-event/passengers-status';
 import { Stop } from 'src/app/classes/data-classes/stop';
@@ -14,26 +14,33 @@ export class StopPositionHandlerService {
 
 	constructor(private stopLookup: StopLookupService, private dateParser: DateParserService) {}
 
+	// Initialise tous les stops de la liste de stop fournie
+	initStops(): void {
+		this.stopLookup.coordinatesIdMapping.forEach((coords: Cartesian3, id: number) => {
+			if (id != 0) {
+				console.log(id);
+				const newStop = new Stop(this.stopLookup.coordinatesFromStopId(id), id.toString());
+				this.stopIdMapping.set(id.toString(), newStop);
+			}
+		});
+	}
+
 	// Ajoute les moments ou les passagers sont présents à un arrêt
 	compileEvents(passengerEvent: PassengerEvent): void {
 		const stopId = passengerEvent.current_location.toString();
+		const stop = this.stopIdMapping.get(stopId);
 
-		if (!this.stopIdMapping.has(stopId)) {
-			const newStop = new Stop(this.stopLookup.coordinatesFromStopId(Number(stopId)), stopId);
-			this.stopIdMapping.set(stopId, newStop);
-		}
-
-		const stop = this.stopIdMapping.get(stopId) as Stop;
-
-		switch (passengerEvent.status) {
-		case PassengersStatus.RELEASE:
-			stop.addPassengerStart(passengerEvent.id, this.dateParser.parseTimeFromString(passengerEvent.time));
-			this.stopIdMapping.set(stopId, stop);
-			break;
-		case PassengersStatus.ONBOARD:
-			stop.setPassengerEnd(passengerEvent.id, this.dateParser.parseTimeFromString(passengerEvent.time));
-			this.stopIdMapping.set(stopId, stop);
-			break;
+		if (stop) {
+			switch (passengerEvent.status) {
+			case PassengersStatus.RELEASE:
+				stop.addPassengerStart(passengerEvent.id, this.dateParser.parseTimeFromString(passengerEvent.time));
+				this.stopIdMapping.set(stopId, stop);
+				break;
+			case PassengersStatus.ONBOARD:
+				stop.setPassengerEnd(passengerEvent.id, this.dateParser.parseTimeFromString(passengerEvent.time));
+				this.stopIdMapping.set(stopId, stop);
+				break;
+			}
 		}
 	}
 
@@ -60,8 +67,8 @@ export class StopPositionHandlerService {
 		viewer.entities.add({
 			position: stop.position,
 			ellipse: {
-				semiMinorAxis: 30,
-				semiMajorAxis: 30,
+				semiMinorAxis: 10,
+				semiMajorAxis: 10,
 				height: 0,
 				material: Cesium.Color.RED,
 				outline: true,
