ee84e41f4d8be84ce3b1a6f2e7c3e7a4aaca2650
Pierre-1Paquette <ppaquette2000@gmail.com>
2023-03-05 15:09:36 -0500

    classe vehicle

---
+++ multimodalsim-vis/frontend/src/app/classes/data-classes/vehicles.ts
@@ -0,0 +1,28 @@
+import { SampledPositionProperty } from 'cesium';
+
+export class Vehicle {
+	private onBoardPassengers = new Array<string>();
+
+	path: SampledPositionProperty;
+	id: string;
+
+	constructor(id: string) {
+		this.id = id;
+		this.path = new Cesium.SampledPositionProperty();
+	}
+
+	addPassenger(id: string): void {
+		this.onBoardPassengers.push(id);
+	}
+
+	removePassenger(id: string): void {
+		const index = this.onBoardPassengers.indexOf(id);
+		if (index > -1) {
+			this.onBoardPassengers.splice(index, 1);
+		}
+	}
+
+	getPassengerAmount(): number {
+		return this.onBoardPassengers.length;
+	}
+}
