08bc24089dfdf6fb16a21a607af0eb18d0eee71e
Rui Jie Li <liruijie1999@gmail.com>
2023-03-18 11:00:15 -0400

    server works

--- multimodalsim-vis/backend/communication/server/app.ts
+++ multimodalsim-vis/backend/communication/server/app.ts
@@ -3,14 +3,14 @@ import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { spawn } from 'node:child_process';
+import { ChildProcessWithoutNullStreams, spawn } from 'node:child_process';
 
 dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const distDir = __dirname + "/dist/";
-const port = process.env['PORT'];
+const port = process.env['PORT'] ? process.env['PORT'] : '8000';
 const app: Express = express();
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", 
@@ -29,11 +29,11 @@ app.listen(port, () => {
 app.get("/api/status", (req: Request, res: Response) =>  {
     res.status(200).json({ status: "UP" });
 });
-
+let runSim:ChildProcessWithoutNullStreams|undefined;
 app.get('/api/start-simulation', (req: Request, res: Response) => {
 	const VENV_PYTHON_PATH = '../../venv/Scripts/python';
-	const runSim = spawn(VENV_PYTHON_PATH, ['../fixed_line_gtfs.py']);
-	
+	runSim = spawn('bash');
+
 	runSim.on('spawn', () => {
 		console.log('Started runSim:');
 	  });
@@ -46,7 +46,25 @@ app.get('/api/start-simulation', (req: Request, res: Response) => {
 		console.log('Running');
 		console.log(`stdout: ${data}`);
 	});
-	
+	runSim.stderr.on('data', (err) => {
+		console.log(`${err}`);
+	} )
+	runSim.stdin.write('cd ../..\n');
+	runSim.stdin.write('source venv/bin/activate\n');
+	runSim.stdin.write('cd communication\n');
+	runSim.stdin.write('py -m fixed_line_gtfs\n');
+	runSim.stdin.end();
 	res.status(200).json({ status: "RUNNING" });
-  });
+});
 
+app.get('/api/pause', (req:Request, res:Response) => {
+	if(runSim) {
+		runSim.kill('SIGTSTP');
+	}
+})
+
+app.get('/api/pause', (req:Request, res:Response) => {
+	if(runSim) {
+		runSim.kill('SIGCONT');
+	}
+})
