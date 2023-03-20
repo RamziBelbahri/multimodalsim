0ee9cedd5c7c9c6b02909d7ba52a55192c8b3666
UrsuleJoelle <ursule-joelle.kanmegne-djoum@polymtl.ca>
2023-03-20 10:02:27 -0400

    prepa merge avec develop pour PR

--- multimodalsim-vis/frontend/src/app/components/sidebar/entity-infos/entity-infos.component.ts
+++ multimodalsim-vis/frontend/src/app/components/sidebar/entity-infos/entity-infos.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Viewer } from 'cesium';
+import { Subscription } from 'rxjs';
+import { EntityLabelHandlerService } from 'src/app/services/cesium/entity-label-handler.service';
+import { ViewerSharingService } from 'src/app/services/viewer-sharing/viewer-sharing.service';
 
 @Component({
-  selector: 'app-entity-infos',
-  templateUrl: './entity-infos.component.html',
-  styleUrls: ['./entity-infos.component.css']
+	selector: 'app-entity-infos',
+	templateUrl: './entity-infos.component.html',
+	styleUrls: ['./entity-infos.component.css']
 })
 export class EntityInfosComponent {
+	private readonly OPTION_PIXEL_SIZE = 49.2;
+	private readonly OPTION_PIXEL_MARGIN = 5;
+
+	private viewer: Viewer | undefined;
+	private viewerSubscription: Subscription = new Subscription();
+	private entityInfosSubscription: Subscription = new Subscription();
+
+	visOptionList: Array<string> = new Array<string>();
+	manipOptionList: Array<string> = new Array<string>();
+
+	lat = 0;
+	lon = 0;
+	passengerAmount = 0;
+	passengerList = new Array<string>();
+
+	constructor(private dialog: MatDialog, private entityHandler: EntityLabelHandlerService, private viewerSharer: ViewerSharingService) {}
+
+	ngOnInit() {
+		this.viewerSubscription = this.viewerSharer.currentViewer.subscribe((viewer) => {
+			this.viewer = viewer;
+
+			this.entityHandler.findClickedEntityId(this.viewer);
+			this.entityInfosSubscription = this.entityHandler.currentEntityInfos.subscribe((infos) => {
+				this.lat = infos.position.x;
+				this.lon = infos.position.y;
+				this.passengerAmount = infos.passengers.length;
+				this.passengerList = infos.passengers;
+			});
+		});
+
+		// this.passengerList.push('Paramètre 1');
+		// this.passengerList.push('Paramètre 2');
+		// this.passengerList.push('Paramètre 3');
+	}
+
+	ngOnDestroy() {
+		this.viewerSubscription.unsubscribe();
+	}
+
+	open(): void {
+		(document.getElementById('sidebar-menu') as HTMLElement).style.width = '340px';
+	}
+
+	close(): void {
+		(document.getElementById('sidebar-menu') as HTMLElement).style.width = '0px';
+	}
 
 }
