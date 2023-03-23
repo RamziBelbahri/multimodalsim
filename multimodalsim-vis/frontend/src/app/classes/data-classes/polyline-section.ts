720535daf626456a01a14f6053afc574bbe7c88d
Pierre-1Paquette <ppaquette2000@gmail.com>
2023-03-23 14:32:06 -0400

    track le progrès

--- multimodalsim-vis/frontend/src/app/classes/data-classes/polyline-section.ts
+++ multimodalsim-vis/frontend/src/app/classes/data-classes/polyline-section.ts
@@ -1,13 +1,15 @@
-import { Cartesian3 } from 'cesium';
+import { Cartesian3, JulianDate } from 'cesium';
 
 export class TimedPolyline {
 	positions: Array<Array<Cartesian3>>;
-	times: Array<Array<number>>;
+	sectionTimes: Array<Array<number>>;
+	times: Array<Array<JulianDate>>;
 	lastSectionCompiled: number;
 
 	constructor() {
 		this.positions = new Array<Array<Cartesian3>>();
-		this.times = new Array<Array<number>>();
+		this.sectionTimes = new Array<Array<number>>();
+		this.times = new Array<Array<JulianDate>>();
 		this.lastSectionCompiled = 0;
 	}
 }
