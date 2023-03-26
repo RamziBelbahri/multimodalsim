63e8d1020504d629f122bc1e6b6896106670454e
UrsuleJoelle <ursule-joelle.kanmegne-djoum@polymtl.ca>
2023-03-26 11:55:16 -0400

    correction bouton infos

--- multimodalsim-vis/frontend/src/app/app.module.ts
+++ multimodalsim-vis/frontend/src/app/app.module.ts
@@ -12,11 +12,12 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatProgressSpinner, MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import {MatIcon, MatIconModule} from '@angular/material/icon';
 import {MatButtonToggle, MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatButtonModule} from '@angular/material/button';
 import { SaveModalComponent } from './components/save-modal/save-modal.component';
 
 @NgModule({
 	declarations: [AppComponent, CesiumContainerComponent, SidebarComponent, DebugReceiverComponentComponent, SimulationModalComponent, SaveModalComponent, EntityInfosComponent],
-	imports: [BrowserModule, BrowserAnimationsModule, MatDialogModule, MatProgressSpinnerModule, HttpClientModule, MatIconModule, MatButtonToggleModule],
+	imports: [BrowserModule, BrowserAnimationsModule, MatDialogModule, MatProgressSpinnerModule, HttpClientModule, MatIconModule, MatButtonToggleModule, MatButtonModule],
 	providers: [MatProgressSpinner, MatIcon, MatButtonToggle],
 	bootstrap: [AppComponent],
 })
