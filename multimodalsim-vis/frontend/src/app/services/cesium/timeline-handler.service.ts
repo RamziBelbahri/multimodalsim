fcb51e258937131eeaba8b2144678f2f73a54562
Pierre-1Paquette <ppaquette2000@gmail.com>
2023-04-14 11:37:28 -0400

    limite de la timeline

---
+++ multimodalsim-vis/frontend/src/app/services/cesium/timeline-handler.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { EntityDataHandlerService } from '../entity-data-handler/entity-data-handler.service';
+import { Viewer } from 'cesium';
+import { DateParserService } from '../util/date-parser.service';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class TimelineHandlerService {
+	constructor(private dataHandler: EntityDataHandlerService, private dateParser: DateParserService) {}
+
+	initBoarding(viewer: Viewer): void {
+		viewer.clock.onTick.addEventListener((clock) => {
+			const currentTime = clock.currentTime;
+			const lastEventTime = this.dateParser.parseTimeFromSeconds(this.dataHandler.combined[this.dataHandler.combined.length - 1].time.toString());
+
+			if (Cesium.JulianDate.greaterThanOrEquals(currentTime, lastEventTime)) {
+				viewer.clock.currentTime = lastEventTime;
+			}
+		});
+	}
+}
